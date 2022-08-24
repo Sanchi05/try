72a53ac9df44b0e3c1729d6015730af931f612df
Sanchi Patel <sanchijp@lentra.ai>
2022-08-24 12:30:36 +0530

    first commit

---
+++ src/main/java/Try1.java
@@ -0,0 +1,27 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.zip.Deflater;
+
+public class Try1 {
+
+    public static void main(String args[]){
+        Date date = new Date();
+
+        System.out.println(date);
+        System.out.println(date);
+
+
+    }
+
+
+}
