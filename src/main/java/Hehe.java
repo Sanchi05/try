7d7bcc14d787af48d4534707878f15b677ab5690
Sanchi Patel <sanchijp@lentra.ai>
2022-10-20 11:47:30 +0530

    first commit

--- src/main/java/Hehe.java
+++ src/main/java/Hehe.java
@@ -1,14 +1,3 @@
 import java.util.Date;
 import java.util.Stack;
 
-public class Hehe {
-    int i ;
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
-    }
-}
--- src/main/java/Try1.java
+++ src/main/java/Try1.java
@@ -1,19 +1,35 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Stream;
 
 public class Try1 {
+    public static void main(String args[]){
+        String date = "9012022";
+        boolean b = validDate(date);
+        System.out.println("boolean "+ b);
 
-    public static String newF(Hehe hehe){
-        hehe.setI(10);
-        return "hehe";
+
     }
-    public static void main(String args[]) throws JsonProcessingException {
-        Hehe hehe = new Hehe();
-        String newStr = newF(hehe);
-        System.out.println(newStr);
-        System.out.println(hehe.i);
+
+    public static boolean validDate(String date){
+        DateFormat sdf_dd = new SimpleDateFormat("ddMMyyyy");
+        DateFormat sdf_d = new SimpleDateFormat("dMMyyyy");
+        sdf_dd.setLenient(false);
+        sdf_d.setLenient(false);
+        try {
+            sdf_dd.parse(date);
+        } catch (ParseException e) {
+            try{
+                sdf_d.parse(date);
+            } catch (Exception exception) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
