e0fa171e78a7b17aa04ec52eaf9f9d988c34c42d
Sanchi Patel <sanchijp@lentra.ai>
2022-10-20 11:58:00 +0530

    first commit

--- src/main/java/Try1.java
+++ src/main/java/Try1.java
@@ -15,6 +15,8 @@ public class Try1 {
 
     }
 
+
+
     public static boolean validDate(String date){
         DateFormat sdf_dd = new SimpleDateFormat("ddMMyyyy");
         DateFormat sdf_d = new SimpleDateFormat("dMMyyyy");
